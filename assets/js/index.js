609f6e678c721ecbadc975d7e58bc16aa1e41240
Kauê Lima <kaue5939.lima@gmail.com>
2024-04-09 17:46:41 -0300

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+const personagens = document.querySelectorAll('.personagem');
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+
+        if(window.innerWidth < 450){
+            window,scrollTo({top: 0, behavior: 'smooth'});
+        };
+        
+        removerSelecaoDoPersonagem();
+
+        personagem.classList.add('selecionado');
+
+        alterarImagemPersonagemSelecionado(personagem);
+
+        alterarNomePersonagemSelecionado(personagem);
+
+        AlterarDescricaoPersonagem(personagem);
+    })
+});
+function AlterarDescricaoPersonagem(personagem) {
+    const descricapPersonagem = document.getElementById('descricao-personagem');
+    descricapPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function alterarNomePersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alterarImagemPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `./assets/img/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoDoPersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
+
